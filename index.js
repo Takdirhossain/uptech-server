74308ffa54f67182ef02e92d5b082e516c8a892c
takdirhossain <takdirhossain35@gmail.com>
2022-11-09 03:23:12 +0600

    complete services

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require("express")
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 5000
+const cors = require('cors')
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("This is uptech server")
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eurlfla.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const addservice = client.db('uptech').collection('service')
+        app.post('/', async (req, res) => {
+            const service = req.body
+            console.log(service)
+            const result = await addservice.insertOne(service)
+            res.send(result)
+        })
+        app.get('/', async (req, res) => {
+            const query = {}
+            const cursor = addservice.find(query).limit(3)
+            const order = await cursor.toArray()
+            res.send(order)
+
+        })
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = addservice.find(query)
+            const order = await cursor.toArray()
+            res.send(order)
+
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.error(error))
+
+
 
 app.listen(port, () => {
     console.log(`server is runing on ${port}`)
